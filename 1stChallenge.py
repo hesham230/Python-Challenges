63266e5f3059a9b27c00317618c6548dd1ca3a72
hesham <hesham2300@gmail.com>
2024-10-25 18:36:48 +0300

    first commit

---
+++ 1stChallenge.py
@@ -0,0 +1,39 @@
+# Program that prints the Max digit, Min digit and the Sum.
+
+num = 3412560879
+
+# Function to extract the maximum digit in a number
+def maxDigit():
+  maximum = 0
+  for digit in str(num):
+    if int(digit) > maximum:
+      maximum = int(digit)
+  print("the maximum is : " , maximum)
+#maxDigit()
+
+# Function to extract the minimum digit in a number
+def minDigit():
+  minimum = 9
+  for digit in str(num):
+    if int(digit) < minimum:
+      minimum = int(digit)
+  print("the minimum is : " , minimum)
+#minDigit()
+
+# Function for displaying the sum of all the digits
+def sumDigits():
+  sumDigits = 0
+  for digit in str(num):
+    sumDigits += int(digit)
+  print("The sum is: ", sumDigits)
+
+#sumDigits()
+
+# Function that prints the Max digit, Min digit and the Sum.
+def maxMinSum():
+  print("The number: ",  num)
+  maxDigit()
+  minDigit()
+  sumDigits()
+
+maxMinSum()
